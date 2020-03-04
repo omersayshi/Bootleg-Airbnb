c08fae1302b64fc92855ec5f78024226ca4bfc8e
omersayshi <brozkiomer@gmail.com>
2020-03-04 18:12:03 -0500

    put the db example in a different js file

---
+++ dbexample.js
@@ -0,0 +1,20 @@
+const {Pool} = require('pg');
+//this is our database connection
+const db = new Pool({
+    user: 'oabub037',
+    host: 'web0.eecs.uottawa.ca',
+    database: 'oabub037',
+    password: 'password',
+    port: 15432,
+});
+
+
+// Test run for what it returns when you run a query 
+// try it out in terminal to check out what it returns in queries 
+//dont use 
+db.query("SELECT * from customer", (err,result)=>{
+    if (err) {
+        return console.error('Error executing query', err.stack)
+      }
+    console.log(result.rows) 
+});
--- index.js
+++ index.js
@@ -1,24 +1,10 @@
-const {Pool} = require('pg');
+const path = require('path');
 express = require('express');
+
 app = express();
-//this is our database connection
-const db = new Pool({
-    user: 'oabub037',
-    host: 'web0.eecs.uottawa.ca',
-    database: 'oabub037',
-    password: 'password',
-    port: 15432,
-});
-
-
-// Test run for what it returns when you run a query 
-// try it out in terminal to check out what it returns in queries 
-db.query("SELECT * from customer", (err,result)=>{
-    if (err) {
-        return console.error('Error executing query', err.stack)
-      }
-    console.log(result.rows) 
-});
+//Now equivalent of body parser
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const PORT = process.env.port || 3000;
 
@@ -27,13 +13,20 @@ app.listen(PORT, ()=>{
 });
 
 
-//Download Postman to test this API endpoints in general
-//This is how to do a GET function
+//Download Postman to test these API endpoints in general
+//This is how to do a GET request
 app.get('/test', (req,res) =>{
-    
+    res.json({
+        name : "Omer",
+        life: false
+    })
 });
+//This how to a POST request
 app.post('/test', (req,res) =>{
-    
+    res.json({
+        name : "Omer",
+        life: false
+    })
 });
 
 
