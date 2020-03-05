0b183baedf32cf8aead9f106df23373c8d1a5068
omersayshi <brozkiomer@gmail.com>
2020-03-05 14:53:53 -0500

    my laptop is going to die

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const path = require('path');
 express = require('express');
 app = express();
 router = express.Router();
+
+//Database
+const {Pool} = require('pg');
+const db = new Pool({
+    user: 'oabub037',
+    host: 'web0.eecs.uottawa.ca',
+    database: 'oabub037',
+    password: 'password',
+    port: 15432,
+});
+
 //Now equivalent of body parser
 //app.use(cors());
 app.use(express.json());
@@ -9,9 +20,19 @@ app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname,'public')));
 
-app.post('/', (req,res)=>{
+app.post('/signin', (req,res)=>{
     const user = req.body;
-    console.log(user)
+    const who = Object.keys(user)[0]
+    if(who == 'guest_id'){
+        
+    }else if(who == 'host_id'){
+        
+    }else if(who == 'employee_id'){
+
+    }else{
+        console.log("Who are you? You are not employee, guest or host loool")
+    }
+    console.log(Object.keys(user)[0] == 'guest_id')
 });
 
 // app.get('/whatever', (req,res)=>{
